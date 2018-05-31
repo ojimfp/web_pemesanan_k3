fda3a8b9c8ce829fc975054a0403ff91280a9808
ITS\5215100059 <rifaihabib29@gmail.com>
2018-05-31 16:06:30 +0700

    halaman pekerja tinggal pembagian pekerja pabrik atau umum

--- user/index.php
+++ user/index.php
@@ -11,6 +11,7 @@ session_start();
 $nip = $_SESSION['nip'];
 $hak_akses = $_SESSION['hak_akses'];
 $nama_karyawan = $_SESSION['nama_karyawan'];
+$tgl = date("d-m-Y");
 
 if($_SESSION['status'] !="login".$nip.""){
   header("location:". $base."login");
@@ -88,7 +89,7 @@ if($_SESSION['status'] !="login".$nip.""){
           </div>
           <div class="panel-body">
             <div class="row">
-              <form action="kirim.php" method="POST">
+              <form action="index.php" method="POST">
                 <table style="margin: 0 auto;" border="0">
                     <!-- <tr>
                       <th>a</th>
@@ -102,13 +103,35 @@ if($_SESSION['status'] !="login".$nip.""){
                         <?php 
                           $i = 0;
                           while ($data = mysqli_fetch_array($read_data)) { ?>
-                        <td style="text-align: center; vertical-align: middle;">
-                          
-                          <img style="width: 140px; margin: 70px 50px 10px;" src="<?php echo $base.'assets/img/'.$data['gambar_apd']; ?>">
-                          <strong><p style="text-align: center;"><?php echo $data['nama_apd'].' - '. $data['id_apd']; ?></p></strong>
-                          <input class="form-control" type="number" name="jumlah" id="jumlah" placeholder="jumlah permintaan">
-                          <input class="form-control" type="hidden" name="id_apd" value="<?php echo $data['id_apd']; ?>">
-                          <input class="form-control" type="checkbox" name="nama_apd" id="id_apd" value="<?php echo $data['nama_apd']; ?>">
+                          <td style="text-align: center; vertical-align: middle;">
+                          <div>
+                            <img style="width: 140px; margin: 70px 50px 10px;" src="<?php echo $base.'assets/img/'.$data['gambar_apd']; ?>">
+                            <strong><p style="text-align: center;"><?php echo $data['nama_apd'].' - '. $data['id_apd']; ?></p></strong>
+                            <!-- <input class="form-control" type="hidden" name="id_apd[]" value="<?php echo $data['id_apd']; ?>"> -->
+                            <input class="form-control" type="number" name="jumlah[]" placeholder="jumlah permintaan" id="<?php echo $data['id_apd'].'num'; ?>" min="0" style="display:none">
+                            <input class="form-control" type="checkbox" name="id_apd[]" value="<?php echo $data['id_apd']; ?>" id="<?php echo $data['id_apd'].'id'; ?>" onclick="<?php echo $data['id_apd'].'()'; ?>">
+                            <!-- <p id="<?php echo $data['id_apd'].'text'; ?>" style="display:none">Checkbox is CHECKED!</p> -->
+                          </div> 
+                          <script> 
+                            function <?php echo $data['id_apd'].'()'; ?> {
+
+                              var checkBox = document.getElementById("<?php echo $data['id_apd'].'id'; ?>");
+                              var input = document.getElementById("<?php echo $data['id_apd'].'num'; ?>");
+                              // var text = document.getElementById("<?php echo $data['id_apd'].'text'; ?>");
+                              
+
+                              if (checkBox.checked == true){
+                                // text.style.display = "block";
+                                // input.hide();
+                                input.style.display = "block";
+                              } else {
+                                input.value = "";
+                                input.style.display = "none";
+                                // text.style.display = "none";
+                              }
+                            }
+
+                          </script>
 
                           <?php 
                           $i++;
@@ -121,7 +144,7 @@ if($_SESSION['status'] !="login".$nip.""){
                       </tr> -->
                 </table>
                 <div style="text-align: center; vertical-align: middle; margin: 40px auto 30px;">
-                  <input class="btn btn-md btn-primary" type="submit" name="" value="Ajukan Permintaan">
+                  <input class="btn btn-md btn-primary" type="submit" name="submit" value="Ajukan Permintaan">
                 </div>
               </form>
             </div>
@@ -130,18 +153,51 @@ if($_SESSION['status'] !="login".$nip.""){
     </div>
   </div>
 
+  
+
+  <!-- <div class="container col-lg-5">
+    <div class="list-group">
+      <form method="POST" action="ganti.php">
+        <div class="panel panel-primary">
+          <div class="panel-heading">
+            <strong>
+              SOP Peminjaman APD
+            </strong>
+          </div>
+          <div class="panel-body">
+            <div class="row" style="margin: 10px; ">
+             <p>Apabila terdapat tamu perusahaan / tamu direksi pada PG. Kebon Agung apabila membutuhkan safety helmet dapat meminjam langsung di gudang persediaan. Berikut meurupakan SOP peminjaman APD untuk tamu perusahaan : </p>
+
+                <p>1.  Tamu perusahaan / Tamu direksi PG. Kebon Agung diperbolehkan meminjam safety helmet pada gudang perusahaan.</p><br>
+                <p>2.  Peminjaman APD dilakukan dengan mengisi formulir peminjaman di website inventory management oleh pekerja/penanggung jawab dari peminjaman.</p><br> 
+                <p>3.  Setelah mengisi formulir peminjaman pada website, peminjam datang ke bagian gudang di damping penanggung jawab barang untuk meminta APD yang telah diisi pada form peminjaman pada website.</p><br>
+                <p>4.  Apabila telah selesai, maka peminjam wajib mengembalikan APD yang dipinjam dengan kondisi seperti semula.</p>
+
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div> -->
+
   <div class="container col-lg-5">
     <div class="list-group">
       <form method="POST" action="ganti.php">
         <div class="panel panel-primary">
           <div class="panel-heading">
             <strong>
-              SOP
+              SOP Permintaan APD
             </strong>
           </div>
           <div class="panel-body">
-            <div class="row">
-             
+            <div class="row" style="margin: 10px; ">
+             <p>Bagi pekerja PG. Kebon Agung apabila APD yang telah diberikan oleh perusahaan rusak akibat proses produksi, maka pekerja dapat meminta APD baru dengan prosedur sebagai berikut : </p>
+
+                <p>1.  Pekerja PG. Kebon Agung hanya diperbolehkan meminta APD yang sesuai dengan ketentuan yang telah ditetapkan perusahaan.</p><br>
+                <p>2.  Pekerja wajib membawa bukti bahwa APD yang digunakan benar benar rusak dan tidak dapat digunakan sehingga membutuhkan pergantian.</p><br> 
+                <p>3.  Dalam melakukan permintaan APD pekerja wajib mengisi formulir permintaan pada website management inventory.</p><br>
+                <p>4.  Setelah mengisi formulir permintaan pekerja datang ke bagian gudang untuk meminta APD yang telah diisi pada form permintaan.</p>
+
             </div>
           </div>
         </div>
@@ -183,6 +239,22 @@ if($_SESSION['status'] !="login".$nip.""){
     </div>
   </div> -->
 
+  <div class="modal fade" tabindex="-1" role="dialog" id="success">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">BERHASIL!</h4>
+        </div>
+        <div class="modal-body">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+
   <script src="<?php echo $base; ?>assets/admin/js/jquery-1.11.1.min.js"></script>
   <script src="<?php echo $base; ?>assets/admin/js/bootstrap.min.js"></script>
   <script src="<?php echo $base; ?>assets/admin/js/chart.min.js"></script>
@@ -202,6 +274,46 @@ if($_SESSION['status'] !="login".$nip.""){
       });
     };
   </script>
+
+<?php 
+  if (isset($_POST['submit'])) {
+  if(!empty($_POST['jumlah'])) {
+    foreach ($_POST['jumlah'] as $key => $value2) {
+    // echo $value2."<br>";
+      if ($value2 !== "") {
+        $jumlah[] = $value2;
+      }
+
+    }
+  }
+
+  if(!empty($_POST['id_apd'])) {
+    foreach ($_POST['id_apd'] as $key1 => $value3) {
+    // echo $value3."<br>";
+      $id[] = $value3;
+    }
+  }
+
+  $j = count($jumlah);
+  $i = count($id);
+
+  if ($j = $i) {
+    for ($a=0; $a < $j; $a++) { 
+      $jumlah_permintaan = $jumlah[$a];
+      $id_apd = $id[$a];
+
+      mysqli_query($conn, "INSERT INTO permintaan(id_permintaan, id_apd, nip_karyawan, tanggal_permintaan, jumlah_permintaan) VALUES ('','$id_apd','$nip','$tgl','$jumlah_permintaan')") or die(mysqli_error());
+    }
+  }
+
+  echo "<script type='text/javascript'>
+   $(window).on('load',function(){
+    $('#success').modal('show');
+    });
+    </script>"; 
+ 
+}
+?>
 
 </body>
 </html>
--- user/kirim.php
+++ user/kirim.php
@@ -4,15 +4,62 @@ include '../config.php';
 
 session_start();
 
-$nama_apd[] = $_POST['nama_apd'];
+// $nama_apd[] = $_POST['nama_apd'];
 $nip = $_SESSION['nip'];
 $tgl = date("d-m-Y");
-$jumlah[] = $_POST['jumlah'];
-$id_apd[] = $_POST['id_apd'];
+// $jumlah[] = $_POST['jumlah'];
+// $id_apd[] = $_POST['id_apd'];
 
-echo $nama_apd[0].'/';
-echo $jumlah[0].'/';
-echo $id_apd[0];
+// echo $nama_apd[0].'/';
+// echo $jumlah[0].'/';
+// echo $id_apd[0];
+
+if(!empty($_POST['jumlah'])) {
+	foreach ($_POST['jumlah'] as $key => $value2) {
+		// echo $value2."<br>";
+		if ($value2 !== "") {
+			$jumlah[] = $value2;
+		}
+		
+	}
+}
+
+if(!empty($_POST['id_apd'])) {
+	foreach ($_POST['id_apd'] as $key1 => $value3) {
+		// echo $value3."<br>";
+		$id[] = $value3;
+	}
+}
+
+
+foreach (array_combine($jumlah, $id) as $jumlah => $id_apd) {
+
+	mysqli_query($conn, "INSERT INTO permintaan(id_permintaan, id_apd, nip_karyawan, tanggal_permintaan, jumlah_permintaan) VALUES ('','$id_apd','$nip','$tgl','$jumlah')") or die(mysqli_error());
+}
+
+echo "<script>alert('Permintaan berhasil dikirim..')</script>";
+echo "<script>location.href='../user';</script>";
+
+// $j = count($jumlah);
+// $i = count($id);
+
+// echo $j."<br>";
+// echo $i."<br><br>";
+
+// for ($a=0; $a < $j; $a++) { 
+// 	echo $jumlah[$a]."<br>";
+// }
+
+// // var_dump($jumlah); echo "<br>";
+
+// for ($b=0; $b < $i; $b++) { 
+// 	echo $id[$b]."<br>";
+// }
+
+// echo $j."<br>";
+// echo $i;
+
+
 
 // $pieces = explode(' ', $nama_apd);
 // $id_apd = array_pop($pieces);
